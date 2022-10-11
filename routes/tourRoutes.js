d859d577d7dfb8ed41ebbf4aa93072ac6318fe98
anuraggiri4308 <86037565+anuraggiri4308@users.noreply.github.com>
2022-10-12 03:14:03 +0530

    Chained checkbody middleware to post request
    line 19: tourRoutes
    .post(tourController.checkBody, tourController.createTour);

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,10 +5,18 @@ const router = express.Router();
 
 router.param('id', tourController.checkId);
 
+//create a checkbody middleware
+
+//check if body contains the name and price property
+
+//if not send back 400 (bad request)
+
+//add it to post handler stack
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour); //chained checkbody so first checkbody middleware will run and then after createTour will run
 router
   .route('/:id')
   .get(tourController.getTourById)
