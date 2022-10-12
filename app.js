10b10b5a8c51de76b5eceda176295223a1ff13a4
anuraggiri4308 <86037565+anuraggiri4308@users.noreply.github.com>
2022-10-12 15:02:25 +0530

    Added environment variable

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-app.use(morgan('dev'));
+// console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.static(`${__dirname}/public`));
 
 app.use(express.json());
 app.use((req, res, next) => {
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const dotenv = require('dotenv');
+// console.log(app.get('env')); //prints development so we are in development env right now
+dotenv.config({ path: './config.env' }); //read config file and save them into nodejs env variables
 const app = require('./app');
 
-const port = 3000;
+// console.log(process.env);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
