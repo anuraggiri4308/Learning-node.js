91f97d2e0daaee28be8ba64b1ac734f7d59e4bd9
anuraggiri4308 <86037565+anuraggiri4308@users.noreply.github.com>
2022-10-30 03:59:28 +0530

    Sorting,field limiting and paggination added

--- tourController.js
+++ tourController.js
@@ -22,7 +22,7 @@ exports.getAllTours = async (req, res) => {
   try {
     //BUILDING QUERY
 
-    //FILTERING
+    //FILTERING(1A)
     const queryObj = { ...req.query }; //we dont want to delete anything from original object so we are using destructuring same as object.assign
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
@@ -37,7 +37,7 @@ exports.getAllTours = async (req, res) => {
     //using REQ.QUERY
     // const tours = await Tour.find(queryObj); //now if we want other query like sort and all then what waits do here is it will send the initial query after sometime so to resolve it we will add this to a query variable and await that query variable in the last see the code below.
 
-    //ADVANCED FILTERING
+    //ADVANCED FILTERING(1B)
     let queryString = JSON.stringify(queryObj);
     queryString = queryString.replace(
       /\b(gte|gt|lte|lt)\b/g,
@@ -47,10 +47,46 @@ exports.getAllTours = async (req, res) => {
     //{ difficulty: 'easy' ,duration: { gte: '5' } }  //output from req.query if we give greater than in the query, only doller sign is missing in the duration value so we will add it
     console.log(JSON.parse(queryString));
 
-    const query = Tour.find(JSON.parse(queryString));
+    let query = Tour.find(JSON.parse(queryString));
+
+    //SORTING(2)
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' '); //http://localhost:3000/api/v1/tours?sort=-price,ratingsAverage
+      // query = query.sort(req.query.sort);  //http://localhost:3000/api/v1/tours?sort=-price
+      query = query.sort(sortBy);
+      console.log(sortBy);
+      //sort('price' ratings) sso if two prices are same we need to sort based on ratings
+    }
+    //if user has not specified any sortings params
+    else {
+      query = query.sort('-createdAt');
+    }
+
+    //FIELD LIMITING(3)   //http://localhost:3000/api/v1/tours?fields=name,duration,difficulty,price
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v'); //it will exclude the v from the json data it is added by mongoose by default
+    }
+
+    //PAGINATION (4)  //http://localhost:3000/api/v1/tours?page=1&limit=3
+
+    const page = req.query.page * 1 || 1; //default page no is 1
+    const limit = req.query.limit * 1 || 100; //default limit is 100
+    const skip = (page - 1) * limit;
+    //page=2&limit=10 , 1-10 page 1 , 11-20 page 2
+    // query = query.skip(10).limit(10); //we need to skip 10 result to get to result no 11 on page 2 (for page=3 we have to skip 20 results)
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments(); //it will count no of items
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
 
     //EXECUTE QUERY
     const tours = await query;
+    //query.sort().select().skip().limit()  all the query is returning a new query so we can chain them
     //2-mongoose method
     // const tours = await Tour.find()
     //   .where('duration')
