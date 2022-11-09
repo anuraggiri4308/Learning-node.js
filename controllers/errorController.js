703044c5c8e26f5e45e2ff07bbcac77212b58c3f
anuraggiri4308 <86037565+anuraggiri4308@users.noreply.github.com>
2022-11-10 02:11:37 +0530

    Handling duplicate database fields
    like in post request if the value is already present in the DB it will give our custom error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,11 @@
 const AppError = require('./../utils/appError');
 
 const handleCastErrorDB = err => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Duplicate field value : ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleDuplicateFieldsDB = err => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
@@ -40,7 +45,10 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
+
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+
     sendErrorProd(error, res);
   }
 };
